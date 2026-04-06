2b68df5fe67e52e598ad9f0fd7935b1a5d753a4b
Jason Ramirez <raiderj77@gmail.com>
2026-04-05 17:51:39 -0700

    feat: add Clarity tracking, IndexNow key, remove Loops.so dead routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -177,7 +177,7 @@ export default function RootLayout({
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window,document,"clarity","script","vss29q2h55");
+            })(window,document,"clarity","script","vsqobt7va0");
           `}
         </Script>
       </head>
