42ce0988ba78def06c81e81d364ee0db2cea0252
raiderj77 <raiderj77@gmail.com>
2026-06-27 10:54:43 -0700

    Switch beehiiv embed to iframe

--- src/components/BeehiivSignup.tsx
+++ src/components/BeehiivSignup.tsx
@@ -1,23 +1,14 @@
-"use client";
-import { useEffect, useRef } from "react";
-
 export default function BeehiivSignup() {
-  const ref = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (!ref.current) return;
-
-    const attr = document.createElement("script");
-    attr.src = "https://subscribe-forms.beehiiv.com/attribution.js";
-    attr.async = true;
-    document.head.appendChild(attr);
-
-    const form = document.createElement("script");
-    form.src = "https://subscribe-forms.beehiiv.com/v3/loader.js";
-    form.setAttribute("data-beehiiv-form", "5c8a2e2f-e544-4cc8-bcdd-2178a16214ec");
-    form.async = true;
-    ref.current.appendChild(form);
-  }, []);
-
-  return <div ref={ref} />;
+  return (
+    <iframe
+      src="https://embeds.beehiiv.com/5c8a2e2f-e544-4cc8-bcdd-2178a16214ec"
+      data-test-id="beehiiv-embed"
+      width="100%"
+      height="52"
+      frameBorder="0"
+      scrolling="no"
+      className="w-full max-w-lg mx-auto block"
+      style={{ border: "none", background: "transparent" }}
+    />
+  );
 }
