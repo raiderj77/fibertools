ba01e33108517b4ed6105538fc94f81778049c7c
raiderj77 <raiderj77@gmail.com>
2026-06-29 17:54:09 -0700

    fix: add React namespace for FormEvent type

--- src/app/yarn-calculator/YarnCalculatorTool.tsx
+++ src/app/yarn-calculator/YarnCalculatorTool.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useMemo, useEffect } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { subscribeToNewsletter } from "@/app/actions/subscribe";
 import Tooltip from "@/components/Tooltip";
 import UnitToggle, { type UnitSystem, useSavedUnits } from "@/components/UnitToggle";
